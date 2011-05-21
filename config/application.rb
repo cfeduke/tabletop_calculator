0bdf8145bde7ad2b1cefb81c6674e02ea5fd8269
Charles Feduke <charles.feduke@gmail.com>
2011-05-21 18:23:41 -0400

    fixture and test framework

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module TabletopCalculator
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+      g.test_framework  :rspec
+    end
   end
 end
