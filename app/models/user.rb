4b96976d9d71a22604b8b9cf985a4451c9df4df4
Charles Feduke <charles.feduke@gmail.com>
2011-05-27 04:32:43 -0400

    devise now has display name and associated features

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :display_name, :presence => true
+  validates_uniqueness_of :display_name, :allow_nil => true, :allow_blank => true, :case_sensitive => false
+  
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :display_name
 end
--- features/army_list.feature
+++ features/army_list.feature
@@ -4,7 +4,6 @@ Feature: user creates army list
 	As a user of the system
 	I want to be able to save an army list
 	
-	@wip
 	Scenario: create an army list
 		Given I am a new, authenticated user
 		When I go to the new_army_list page
