39335e38ab32e7caecff2adbcf4af02eeb26b60f
Charles Feduke <charles.feduke@gmail.com>
2011-06-01 15:28:42 -0400

    more work on showing an army list

--- spec/views/army_lists/show.html.haml_spec.rb
+++ spec/views/army_lists/show.html.haml_spec.rb
@@ -4,9 +4,14 @@ describe "army_lists/show.html.haml" do
   subject do
     assign(:army_list, 
       stub_model(ArmyList, 
+        :name => "Name",
         :user => User.new do |u|
           u.display_name = "John Smith"
           u.id = 1
+        end,
+        :game_system => GameSystem.new do |gs|
+          gs.id = 1
+          gs.name = "Generic Game System"
         end
       )
     )
@@ -15,4 +20,6 @@ describe "army_lists/show.html.haml" do
   end
   
   it { should have_tag("a", :text => "John Smith") }
+  it { should have_tag("p", :text => /Name/m) }
+  it { should have_tag("a", :text => /Generic Game System/m) }
 end
